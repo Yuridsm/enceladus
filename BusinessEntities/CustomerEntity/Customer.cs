7a10e459ba45e55b166333400177d9a3ecaa33bc
Yuri Melo <yurifullstack@gmail.com>
2021-08-30 14:35:58 -0300

    BusinessEntities

---
+++ BusinessEntities/CustomerEntity/Customer.cs
@@ -0,0 +1,21 @@
+﻿using BusinessContracts;
+using System;
+
+namespace BusinessEntities.CustomerEntity
+{
+    public class Customer : Identifier
+    {
+        public override Guid IdentifierOfEntity 
+        { 
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = Guid.NewGuid();
+            }
+        }
+        public string Name { get; set; }
+    }
+}
---
+++ BusinessEntities/DriverEntity/Driver.cs
@@ -0,0 +1,24 @@
+﻿using BusinessContracts;
+using BusinessEntities.CustomerEntity;
+using System;
+using System.Collections.Generic;
+
+namespace BusinessEntities.DriverEntity
+{
+    public class Driver : Identifier
+    {
+        public override Guid IdentifierOfEntity
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = Guid.NewGuid();
+            }
+        }
+        public string Name { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
