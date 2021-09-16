cf0246de2e330937aece7a90e19df30c1a543a56
Yuri Melo <yurifullstack@gmail.com>
2021-09-15 23:19:42 -0300

    WorkingStatistics contract

---
+++ BusinessContracts/WorkingStatistics/IWorkingStatistics.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessContracts.WorkingStatistics
+{
+    public interface IWorkingStatistics
+    {
+        public decimal HourSalary { get; set; }
+        public int WorkingHours { get; set; }
+        public decimal MonthSalary { get; set; }
+        public bool PayHourly { get; set; }
+    }
+}
