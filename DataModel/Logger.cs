d06048d1d084cb7f5dab60da5d278afa301655f2
Yuri Melo <yurifullstack@gmail.com>
2021-09-15 23:58:45 -0300

    Logger

---
+++ DataModel/Logger.cs
@@ -0,0 +1,13 @@
+﻿using BusinessContracts.Logger;
+using System.IO;
+
+namespace DataModel
+{
+    public class Logger : ILogger
+    {
+        public void Info(string s)
+        {
+            File.WriteAllText(@"C:\log.txt", s);
+        }
+    }
+}
