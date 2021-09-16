a2a79b5a0d1c8bad2179efe196cf25ef03a2fe12
Yuri Melo <yurifullstack@gmail.com>
2021-09-15 23:20:12 -0300

    Working Statistic entity

---
+++ BusinessEntities/WorkingStatisticsEntity/WorkingStatistics.cs
@@ -0,0 +1,12 @@
+﻿using BusinessContracts.WorkingStatistics;
+
+namespace BusinessEntities.WorkStatisticEntity
+{
+    public class WorkingStatistics : IWorkingStatistics
+    {
+        public decimal HourSalary { get; set; }
+        public int WorkingHours { get; set; }
+        public decimal MonthSalary { get; set; }
+        public bool PayHourly { get; set; }
+    }
+}
