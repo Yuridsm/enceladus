aa3ccfd7b6a119577ec6b1a2ffc405dcba74fde0
Yuri Melo <yurifullstack@gmail.com>
2021-09-19 23:28:33 -0300

    DbGateway Fake

---
+++ Business.Tests/TestDoubles/DbGatewayFake.cs
@@ -0,0 +1,25 @@
+﻿using BusinessContracts.DbGateway;
+using BusinessContracts.WorkingStatistics;
+using BusinessEntities.WorkStatisticEntity;
+using System;
+using System.Collections.Generic;
+
+namespace Business.Tests.TestDoubles
+{
+    public class DbGatewayFake : IDbGateway
+    {
+        private Dictionary<int, IWorkingStatistics> _storage = new Dictionary<int, IWorkingStatistics>() 
+        {
+            { 1, new WorkingStatistics() { PayHourly = true, WorkingHours = 5, HourSalary = 10 } },
+            { 2, new WorkingStatistics() { PayHourly = false, WorkingHours = 500 } },
+            { 3, new WorkingStatistics() { PayHourly = true, WorkingHours = 5, HourSalary = 100 } },
+            { 4, new WorkingStatistics() { PayHourly = false, WorkingHours = 20, HourSalary = 90 } }
+        };
+        public bool Connected => throw new NotImplementedException();
+
+        public IWorkingStatistics GetWorkingStatistics(int id)
+        {
+            return _storage[id];
+        }
+    }
+}
