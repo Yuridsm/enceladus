fb8c96949264a535c7e8f2797935189b6937f28b
Yuri Melo <yurifullstack@gmail.com>
2021-09-19 23:28:40 -0300

    FileOnMachine contract

---
+++ BusinessContracts/FileOnMachine/IFileOnMachine.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessContracts.FileOnMachine
+{
+    public interface IFileOnMachine
+    {
+        void Save(string text);
+        void SetDirectory(string path);
+        void SetFileName(string filename);
+        bool IsValid(string path);
+        byte[] ReadFile(string path);
+    }
+}
