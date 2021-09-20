683d6defe0fd004109999677a08430f35a49ad6f
Yuri Melo <yurifullstack@gmail.com>
2021-09-19 23:28:54 -0300

    Socket data model

---
+++ DataModel/Socket.cs
@@ -0,0 +1,22 @@
+﻿using BusinessContracts.Socket;
+
+namespace DataModel
+{
+    public class Socket : ISocketOverNetwork
+    {
+        public void sendMessage(byte[] message)
+        {
+            // Do nothing
+        }
+
+        public void SetDNS(string dns)
+        {
+            // Do nothing
+        }
+
+        public void SetHost(string host)
+        {
+            // Do nothing
+        }
+    }
+}
