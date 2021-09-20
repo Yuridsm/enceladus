f630cdea9f1b20b5d69f8946a9b00430a6cb7950
Yuri Melo <yurifullstack@gmail.com>
2021-09-19 23:28:37 -0300

    DbGateway Mock

---
+++ Business.Tests/TestDoubles/DbGatewayMock.cs
@@ -0,0 +1,29 @@
+﻿using BusinessContracts.DbGateway;
+using BusinessContracts.WorkingStatistics;
+
+namespace Business.Tests.TestDoubles
+{
+    public class DbGatewayMock : IDbGateway
+    {
+        public bool Connected => false;
+        public int Id { get; set; }
+        private IWorkingStatistics _ws;
+
+        public IWorkingStatistics GetWorkingStatistics(int id)
+        {
+            Id = id;
+            return _ws;
+        }
+
+        public void SetWorkingStatistics(IWorkingStatistics ws)
+        {
+            _ws = ws;
+        }
+
+        public bool VerifyCalledWithProperId(int id)
+        {
+            return Id == id;
+        }
+
+    }
+}
